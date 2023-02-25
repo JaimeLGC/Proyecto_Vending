5fd04178e162a186c866eda5f6a5e3f7035c4456
YerobeGG <zlifeinvader@gmail.com>
2023-02-25 17:02:46 +0000

    Añadida comprobación de si la cantidad pagada es mayor, menor o igual a lo que hay que pagar, falta resolver el problema con el cambio

--- vending_y.py
+++ vending_y.py
@@ -22,8 +22,8 @@ must_pay = 0
 machine = [["water", "0.5"],["cookies","1"],["coke","2"],["snacks","1.50"],["fries", "1"]]
 for product in machine:
     print(product)
-# Función para la acción de realizar un pedido
-def _request(product, stock):
+# Función para la acción de realizar un pedido y pagar
+def _request(product, _return):
     for product in order.keys():
         # Revisión si el producto esta en stock o no
         if product not in prices_and_stock:
@@ -34,5 +34,27 @@ def _request(product, stock):
         # Si tanto el stock y la cantidad estan bien calculamos lo que debemos pagar
         else:
             must_pay += prices_and_stock.get(product).get("price") * order.get(product)
+    # Crearemos un print para simular cuanto va a pagar el cliente
+    payment = int(input("Introduzca la cantidad suficiente para pagar su pedido por favor; "))
+    print(payment)
+    # Analizamos si la cantidad dada es superior, igual o inferior
+    if payment > must_pay:
+        _return = payment - must_pay
+        print("Gracias por su pedido!, Enseguida le devolvemos su cambio.")
+    elif payment == must_pay:
+        print("Gracias por su pedido!")
+    else:
+        print("La cantidad introducida es insuficiente, intentelo de nuevo")
+        print("payment")
+    return _return
 
-        
+# Diccionario con menodas de devolución y la cantidad que tenemos de ellas
+currency_and_stock = {
+    "0.5euro": {"stock": 30},
+    "1euro": {"stock": 20},
+    "2euro": {"stock": 15}
+}
+
+# Función para la acción de devolver el cambio
+def exchange(_return):
+    
