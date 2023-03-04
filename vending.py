94fc89a547ec51ed336b33fb9cd3893edc142d8a
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-04 16:19:54 +0000

    actualizacion vending data

--- vending.py
+++ vending.py
@@ -1,15 +1,26 @@
-#  **********************
-#  * MÁQUINA DE VENDING *
-#  **********************
+# ******************
+# MÁQUINA DE VENDING
+# ******************
+import filecmp
+from pathlib import Path
 
-# FUNCIÓN DE LECTURA
 
-# FUNCIÓN DE ESCRITURA
+def run(operations_path: Path) -> bool:
+    status_path = "data/vending/status.dat"
+    # FUNCIÓN DE LECTURA
+    # def lectura():
+    # FUNCIÓN DE ESCRITURA
 
-# FUNCIÓN DE PEDIDO
+    # FUNCIÓN DE PEDIDO
 
-# FUNCIÓN DE REPOSICIÓN DE PRODUCTO
+    # FUNCIÓN DE REPOSICIÓN DE PRODUCTO
 
-# FUNCIÓN DE CAMBIO DE PRECIO
+    # FUNCIÓN DE CAMBIO DE PRECIO
 
-# FUNCIÓN DE REPOSICION DE DINERO
+    # FUNCIÓN DE REPOSICION DE DINERO
+
+    return filecmp.cmp(status_path, "data/vending/.expected", shallow=False)
+
+
+if __name__ == "__main__":
+    run("data/vending/operations.dat")
